c9da89ba1304a42c4e8e55da0aee15ca7af14c12
denilsonpy <contato.denilsonsilva@gmail.com>
2023-02-21 10:45:45 -0300

    feat: adds datasets

--- main.js
+++ main.js
@@ -1,22 +1,17 @@
+var fs = require("fs");
 var BayesClassifier = require("./bayes-classifier");
 var classifier = new BayesClassifier();
 
-var positiveDocuments = [
-  "I love tacos.",
-  "Dude, that burrito was epic!",
-  "Holy cow, these nachos are so good and tasty.",
-  "I am drooling over the awesome bean and cheese quesadillas.",
-];
-
-var negativeDocuments = [
-  "Gross, worst taco ever.",
-  "The buritos gave me horrible diarrhea.",
-  "I'm going to puke if I eat another bad nacho.",
-  "I'd rather die than eat those nasty enchiladas.",
-  "I hate tacos.",
-];
-
+var positiveDocument = fs.readFileSync(
+  __dirname + "/data-sets/positive-words.txt"
+);
+var positiveDocuments = positiveDocument.toString().split("\n");
 classifier.addDocuments(positiveDocuments, "positive");
+
+var negativeDocument = fs.readFileSync(
+  __dirname + "/data-sets/negative-words.txt"
+);
+var negativeDocuments = negativeDocument.toString().split("\n");
 classifier.addDocuments(negativeDocuments, "negative");
 
 classifier.train();
@@ -27,3 +22,5 @@ console.log(classifier.classify("The torta is epicly bad.")); // "negative"
 console.log(classifier.classify("The torta is horribly awesome.")); // "positive"
 console.log(classifier.classify("I love you so much.")); // "positive"
 console.log(classifier.classify("I hate torta.")); // "negative"
+console.log(classifier.classify("I really hate tacos.")); // "negative"
+console.log(classifier.classify("My mon is amazing i love him.")); // "positive"
